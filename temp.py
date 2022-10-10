7cfc93c8129a4e5b60fbf6536a43f81c0338cf34
Orbel B <o.baghdasian@gmail.com>
2022-10-10 13:17:33 -0700

    must fix oversampling issue

--- temp.py
+++ temp.py
@@ -1,18 +1,16 @@
 import pandas as p
-import csv
-test =  p.read_csv('./data_files/test.csv')
-test2 = p.read_csv('./data_files/test2.csv')
-test.head(8)
-test2.head()
-test["app"] = ""
+import numpy as np
+df_app = p.read_csv('./data_files/application_record.csv')
+df_credit = p.read_csv('./data_files/credit_record.csv')
 
-with open('test2.csv', 'r') as f:          # Read lines separately
-    reader = csv.reader(f, delimiter='t')
-    for i, line in enumerate(reader):
-        print(i, line)
+#credit card application: 
+#print(df_app.head(5))
 
 
 
+
+a = df_credit['STATUS'].value_counts()
+print(a)
 
 
 
@@ -20,11 +18,56 @@ with open('test2.csv', 'r') as f:          # Read lines separately
 
 
 
-def classify():
-    df_app_rec['approved'] =''
-    cred_id = df_cred_rec['ID'].unique().copy()
-    app_id = df_cred_rec['ID'].copy()
-    for row in cred_id:
-        print(row)
-        print(df_cred_rec.where(df_cred_rec["ID"] == row) == True)
-classify()
+
+
+
+
+
+
+
+
+
+
+
+#will not be used, going with method twoLF will be replaced by CRLF the next time Git touches it
+
+#this function compares the id of df_app_rec & cred_id to see if they were approved
+#saves who was approved and returns an array of boolean values. 
+#the index represent the specific row's boolean value
+# def get_passed_app():
+#     cred_id = df_credit['ID'].unique().copy()
+    
+#     app_id = df_app["ID"].copy()
+    
+#     bool_arr = np.isin(app_id, cred_id)
+#     return np.array(bool_arr)
+# classes = get_passed_app()
+# app_length = len(classes)
+# app_length
+
+
+# with open('test2.csv', 'r') as f:          # Read lines separately
+#     reader = csv.reader(f, delimiter='t')
+#     for i, line in enumerate(reader):
+#         print(i, line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def classify():
+#     df_app_rec['approved'] =''
+#     cred_id = df_cred_rec['ID'].unique().copy()
+#     app_id = df_cred_rec['ID'].copy()
+#     for row in cred_id:
+#         print(row)
+#         print(df_cred_rec.where(df_cred_rec["ID"] == row) == True)
+# classify()
